a26f13d8ea3b8c34c126a900ec7ead1886fe3c63
tientran04 <66058866+tientran04@users.noreply.github.com>
2023-05-15 18:16:13 +0700

    Add files via upload

--- US_Crime_SQL_Script.sql
+++ US_Crime_SQL_Script.sql
@@ -1,16 +1,16 @@
-/* 
-1. INTRODUCTION
-In this project, I will apply my SQL skill to perform Exploratory Data Analysis.
-Skills covered in this project include data import, data manipulation and analysis.
 
-2. DATASET
-The dataset used in this project is the Crime Dataset of the City of Los Angeles from 2020 until April 2023.
-Data source: https://catalog.data.gov/dataset/crime-data-from-2020-to-present
+-- 1. INTRODUCTION
+-- Crimes have become a problem in morden society since there are more and more crimes nowadays.
+-- In this project, I will apply SQL skills to analyze crime dataset in order to understand the behaviours of criminals.
 
-3. DATA PREPARATION
-Since the data is stored in CSV file, I need to create a table in SQL and import the data from CSV file.
-Below are sripts that I used to create table and import data.
+-- 2. DATASET
+-- The dataset used in this project is the Crime Dataset of the City of Los Angeles from 2020 until present.
+-- Data source: https://catalog.data.gov/dataset/crime-data-from-2020-to-present
 
+-- 3. DATA PREPARATION
+-- 3.1. DATA IMPORT
+-- Since the data is stored in CSV file, I will create a table in SQL and import the data from CSV file.
+-- Below are sripts that I used to create table and import data.
 -- Create table
 CREATE TABLE IF NOT EXISTS us_crime (
 	dr_no VARCHAR(10),
@@ -43,102 +43,137 @@ CREATE TABLE IF NOT EXISTS us_crime (
 	lon FLOAT,
 	PRIMARY KEY (dr_no)
 	)
-
+	
 -- Import data
 COPY us_crime
 FROM 'C:\Users\Admin\Desktop\Case Study\US Crime\Crime_Data_from_2020_to_Present.csv'
 DELIMITER ','
 CSV HEADER;
-*/
+
 
 -- 4. DATA MANIPULATION AND ANALYSIS
+-- Since the year 2023 is still going, I will exclude 2023 data from my analysis.
 
--- 4.1. NUMBER OF CRIME CASES IN THE DATASET
-
-SELECT COUNT(dr_no)
-FROM us_crime;
-
--- There are 690,454 crime cases occured in the City of Los Angeles from 2020 until April 2023.
-
-
--- 4.2. TOTAL CRIME CASES IN 2020, 2021 AND 2022
-
-SELECT DATE_PART('year', date_occ) AS year_occ, COUNT(dr_no) AS total_cases
-FROM us_crime
-WHERE DATE_PART('year', date_occ) != '2023'
-GROUP BY year_occ;
-
--- The number of crime cases increased over years from 100,057 cases in 2020 to 232,443 cases in 2022.
-
-
--- 4.3. DAILY CASES IN EACH YEAR
-
-WITH date_case AS (
-	SELECT DATE_PART('year', date_occ) AS year_occ, date_occ, COUNT(dr_no) AS total_case
+-- 4.1. CRIME CASES AND PERCENTAGE CHANGE FROM 2020 TO 2022
+SELECT  year_occ, 
+		total_cases, 
+		(total_cases*1.0/ FIRST_VALUE(total_cases) OVER(ORDER BY year_occ) - 1) AS pct_compared_2020
+FROM
+(
+	SELECT  DATE_PART('year', date_occ) AS year_occ, 
+			COUNT(dr_no) AS total_cases
 	FROM us_crime
-	GROUP BY date_occ, year_occ)
+	WHERE DATE_PART('year', date_occ) BETWEEN 2020 AND 2022
+	GROUP BY year_occ
+) a;
 
-SELECT year_occ, ROUND(AVG(total_case)) AS daily_case
-FROM date_case
-GROUP BY year_occ
-
--- Daily cases increased from 544 cases in 2020 to 637 cases in 2022.
-
-
--- 4.4. TIME OF THE DAY THAT CRIMINALS TEND TO COMMIT TO A CRIME
-
-/* In order to analyze crime cases based on time, I will classify time into 4 groups as followings and 
-analyze the data according to these group.
-Time in a day:
-- Morning: from 06:00 to 11:59.
-- Afternoon: from 12:00 to 17:59.
-- Evening: from 18:00 to 23:59.
-- Night: from 00:00 to 05:59.
-*/
-
-SELECT 
-	CASE
-		WHEN time_occ BETWEEN '06:00:00' AND '11:59:00' THEN 'Morning'
-		WHEN time_occ BETWEEN '12:00:00' AND '17:59:00' THEN 'Afternoon'
-		WHEN time_occ BETWEEN '18:00:00' AND '23:59:00' THEN 'Evening'
-		WHEN time_occ BETWEEN '00:00:00' AND '05:59:00' THEN 'Night'
-		END AS time_group,
-	COUNT(dr_no) AS total_cases
-FROM us_crime
-GROUP BY time_group;
-
--- During the day, criminals tend to commit to a crime in the afternoon and evening more than in morning and night time.
+-- The number of crime cases increased from 199,057 cases in 2020 to 232,443 cases in 2022.
+-- Compared to 2020, the total cases in 2021 increased 4.94% and the total cases in 2022 increased 16.77%.
 
 
--- 4.5. TOP 5 AREAS THAT CRIME OCCURED THE MOST IN 2022
+-- 4.2 MONTHLY CASES ANALYSIS
+SELECT  month_occ,
+		MAX(CASE WHEN year_occ = 2020 THEN total_cases END) AS year_2020,
+		MAX(CASE WHEN year_occ = 2021 THEN total_cases END) AS year_2021,
+		MAX(CASE WHEN year_occ = 2022 THEN total_cases END) AS year_2022,
+		SUM(total_cases) AS total_cases,
+		AVG(total_cases) AS avg_cases
+FROM
+(
+	SELECT  DATE_PART('year', date_occ) AS year_occ,
+			DATE_PART('month', date_occ) AS month_occ,
+			COUNT(dr_no) AS total_cases
+	FROM us_crime
+	WHERE DATE_PART('year', date_occ) BETWEEN 2020 AND 2022
+	GROUP BY 1, 2
+) a
+GROUP BY 1
+ORDER BY 1;
 
-SELECT area_name, 
-	   COUNT(dr_no) AS total_cases, 
-	   ROUND(COUNT(dr_no) * 100.0 / (SELECT COUNT(dr_no) FROM us_crime 
-									 WHERE DATE_PART('year', date_occ) = '2022'), 2) AS percentage
-FROM us_crime
-WHERE DATE_PART('year', date_occ) = '2022'
-GROUP BY area_name
-ORDER BY total_cases DESC
-LIMIT 5;
+-- The cases fluctuates over months, so there is no correlation between month and cases happened.
+
+
+-- 4.3. WEEKDAY CASES ANALYSIS
+SELECT  weekday,
+		day_name,
+		avg_cases,
+		avg_cases - MIN(avg_cases) OVER() AS diff_vs_min,
+		avg_cases*1.0/MIN(avg_cases) OVER() - 1 AS diff_vs_min_pct
+FROM
+(
+	SELECT  weekday,
+			day_name,
+			AVG(total_cases) AS avg_cases
+	FROM
+	(
+		SELECT  date_occ,
+				EXTRACT( ISODOW FROM date_occ) AS weekday,
+				TO_CHAR(date_occ, 'Day') as day_name,
+				COUNT(dr_no) AS total_cases
+		FROM us_crime
+		WHERE DATE_PART('year', date_occ) BETWEEN 2020 AND 2022
+		GROUP BY 1, 2, 3
+	) a
+	GROUP BY 1, 2
+	ORDER BY 1
+) aa
+ORDER BY 1;
+
+
+-- Friday is the day of the week that has most cases, followed by Saturday.
+
+
+-- 4.4. HOURLY CASE ANALYSIS
+SELECT  hour_occ,
+		AVG(total_cases) AS avg_cases
+FROM
+(
+	SELECT  date_occ,
+			DATE_PART('hour', time_occ) AS hour_occ,
+			COUNT(dr_no) AS total_cases
+	FROM us_crime
+	WHERE DATE_PART('year', date_occ) BETWEEN 2020 AND 2022
+	GROUP BY 1, 2
+) a
+GROUP BY 1
+ORDER BY 1;
+
+-- Criminals tend to commit to a crime at 12PM until midnight.
+
+
+-- 4.5. DISTRIBUTION BY AREA IN 2022
+SELECT  area_name,
+		total_cases,
+		total_cases*1.0/SUM(total_cases) OVER(PARTITION BY year_occ) AS pct
+FROM
+(
+	SELECT  DATE_PART('year', date_occ) AS year_occ,
+			area_name, 
+			COUNT(dr_no) AS total_cases
+	FROM us_crime
+	WHERE DATE_PART('year', date_occ) = 2022
+	GROUP BY 1,2 
+	ORDER BY 1, 3 DESC
+) a;
 
 -- In 2022, Central is the area that has most frequent crime with 7.56% of total cases, followed by 77th Street, Southwest, Pacific and Hollywood areas.
 
 
--- 4.6. TOP 5 CRIME TYPES
-
-SELECT crm_cd_desc, COUNT(dr_no) AS total_cases
+-- 4.6. TOP FIVE CRIME TYPES IN 2022
+SELECT  crm_cd_desc, 
+		COUNT(dr_no) AS total_cases,
+		COUNT(dr_no)*1.0/(SELECT COUNT(dr_no) FROM us_crime 
+					  WHERE DATE_PART('year', date_occ) = 2022) AS pct
 FROM us_crime
---WHERE DATE_PART('year', date_occ) = '2022'
-GROUP BY crm_cd_desc
-ORDER BY total_cases DESC
+WHERE DATE_PART('year', date_occ) = 2022
+GROUP BY 1
+ORDER BY 2 DESC
 LIMIT 5;
 
--- From 2022 until present, Vehicle Stolen is the most frequent crime type happened in the City of Los Angeles with 74,370 cases.
+-- In 2022, Vehicle Stolen is the most frequent crime type happened, accounted for 10.61% of total cases, followed by Theft of Identity, Battery - Simple Assault, Burglary From Vehicle and Burglary.
 
 
--- 4.7. VICTIM DISTRIBUTION BY AGE GROUP
-
+-- 4.7. VICTIM DISTRIBUTION BY AGE GROUP IN 2022
 /* I will classify age into following groups for this analysis.
 - Children: 1 to 12 years old
 - Adolescents: 13 to 17 years old
@@ -154,25 +189,26 @@ SELECT
 		WHEN vict_age BETWEEN 18 AND 35 THEN 'Young Adults'
 		WHEN vict_age BETWEEN 36 AND 55 THEN 'Middle-aged Adults'
 		WHEN vict_age > 55 THEN 'Older Adults'
-		END AS age_group,
+	END AS age_group,
 	COUNT(dr_no) AS total_cases,
-	ROUND(COUNT(dr_no) * 100.0 / (SELECT COUNT(dr_no) FROM us_crime WHERE vict_age > 0), 2) AS percentage
+	COUNT(dr_no)* 1.0/(SELECT COUNT(dr_no) FROM us_crime WHERE vict_age > 0
+								 AND DATE_PART('year', date_occ) = 2022) AS pct
 FROM us_crime
-WHERE vict_age > 0
-GROUP BY age_group
-ORDER BY percentage DESC;
+WHERE vict_age > 0 AND DATE_PART('year', date_occ) = 2022
+GROUP BY 1
+ORDER BY 3 DESC;
 
--- Young Adults is the most target victims for criminals with 43.54%, followed by Middle-aged Adults with 35.91% and Older Adults with 17.12%.
+-- In 2022, Young Adults is the most target victims for criminals with 43.92%, followed by Middle-aged Adults with 35.68% and Older Adults with 16.89%.
 
 
--- 4.8. TOP 5 POPULAR WEAPONS
-
-SELECT weapon_desc, COUNT(dr_no) AS total_cases,
-	   ROUND(COUNT(dr_no) * 100.0 / (SELECT COUNT(dr_no) FROM us_crime), 2) AS percentage
-FROM us_crime
-WHERE weapon_desc IS NOT NULL
-GROUP BY weapon_desc
-ORDER BY total_cases DESC
-LIMIT 5;
-
--- The most popular weapon that criminals used the most is their arm, followed by other weapons, verbal threat, hand gun and semi-automatic pistol.
+-- 5. CONCLUSIONS
+/* From the analysis, there are useful insights as followings.
+- Crime cases have increased more in 2022 compared with 2021.
+- There is no clear relationship between crime and month during a year.
+- Friday and Saturday are two days with the most crimes.
+- Criminals tend to commit to a crime at 12PM until midnight.
+- In 2022:
+	Central, 77th Street, Southwest, Pacific and Hollywood are top five ares having the most cases, accounted for almost 30% of total cases.
+	Vehicle Stolen and Theft of Identity are the two most happened crime types, accounted for nearly 20% of total cases.
+	Young Adults (age from 18 to 35) and Middle-aged Adults (age from 36 to 55) are the two most target victims, accounted for around 80% of total cases.
+*/
